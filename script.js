ef72784b917661caf62cd3405c50d4c292f0aa30
WiRo96 <wilrob919@gmail.com>
2019-01-24 16:11:38 +0100

    remplacement onclick

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function pop(div) {
-	document.getElementById(div).style.display='block';
-	return false;
+function pop() {
+	this.style.display='block';
 }
-function hide(div) {
-	document.getElementById(div).style.display='none';
-	return false;
+function hide() {
+	this.style.display='none';
+}
+
+var hd = document.getElementById("popped");
+for (var i = 0; i < hd.length; i++) {
+	hd[i].addEventListener("click", hide);
+	console.log(hd[i]);
+}
+
+var pp = document.getElementById("hidden");
+for (var i = 0; i < pp.length; i++) {
+	pp[i].addEventListener("click", pop);
+	console.log(pp[i]);
 }
--- script2.js
+++ script2.js
@@ -1,12 +1,15 @@
 function nicebut() {
-		this.style.background = "red";
-		this.style.color = "blue";
+	this.style.background = "red";
+	this.style.color = "blue";
 }
 function norbut() {
-	this.style.background = "blue";
+  this.style.background = "blue";
 	this.style.color = "red";
 }
 
 var stybut = document.getElementsByTagName('button');
-stybut.getEventListener("mouseover", nicebut);
-stybut.getEventListener("mouseout", norbut);
+console.log(stybut);
+for (var i = 0; i < stybut.length; i++) {
+  stybut[i].addEventListener("mouseover", nicebut);
+  stybut[i].addEventListener("mouseout", norbut);
+}
