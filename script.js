200cc9fb3cedba478ae308eea916c8f73621d9cf
WiRo96 <wilrob919@gmail.com>
2019-01-25 15:13:50 +0100

    création formulaire responsive

--- script.js
+++ script.js
@@ -5,6 +5,45 @@ function hide() {
 	document.getElementById('pop1').style.display='none';
 }
 
-document.getElementById("hd").addEventListener("click", pop);
+document.getElementById('hd').addEventListener("click", pop);
 
-document.getElementById("pp").addEventListener("click", hide);
+document.getElementById('pp').addEventListener("click", hide);
+
+
+function disbut() {
+	document.getElementById('subm').disabled = true;
+	document.getElementById('subm').style.background = "grey";
+	document.getElementById('subm').style.color = "black";
+	document.getElementById('subm').removeEventListener("mouseover", nicebut);
+	document.getElementById('subm').removeEventListener("mouseout", norbut);
+}
+function enbut() {
+	document.getElementById('subm').disabled = false;
+	document.getElementById('subm').style.background = "blue";
+	document.getElementById('subm').style.color = "red";
+	document.getElementById('subm').addEventListener("mouseover", nicebut);
+	document.getElementById('subm').addEventListener("mouseout", norbut);
+}
+
+function substate() {
+	if (ismail()) {
+		return enbut()
+	} else {
+		return disbut()
+	}
+}
+
+function ismail() {
+	var entm = mailval.value;
+	var result = false;
+	for (var i = 0; i < entm.length; i++) {
+		if (entm[i] == "@") {
+			result = true;
+		}
+	}
+	return result;
+}
+
+var mailval = document.getElementById('mailin');
+mailval.addEventListener("input", substate);
+substate()
